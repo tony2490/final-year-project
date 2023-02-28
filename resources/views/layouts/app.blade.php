56165ea99bbf1c87ff7f97b8ca1fb14521f3a1bb
tony2490 <tonyyugbovwre@gmail.com>
2023-02-28 18:10:35 +0000

    Updated Navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,6 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Aston University Flex</title>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -17,92 +15,91 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Aston Part Time</title>
+    
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-<!--         
-    <a class="navbar-brand" href="#"> {{config('app.name', 'Aston Flex') }} </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button> -->
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="/jobs">Jobs</a>
-        </li>
-        @cannot('is-admin')
-        <li class="nav-item">
-            <a class="nav-link" href="/applications/myApplications">My Applications</a>
-        </li>
-        @endcannot
-
-        @can('is-admin')
-        <li class="nav-item">
-            <a class="nav-link" href="/applications">Applications</a>
-        </li>
-       
-        <li class="nav-item">
-            <a class="nav-link" href="{{route('admin.users.index') }} ">Users</a>
-        </li>
-        <li class="nav-item active">
-            <a class="nav-link" href="/jobs/create">Create A Job</a>
-        </li>
-        @endcan
-        </ul>
-    
-           
-    </div>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
       
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav me-auto">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/jobs">Jobs <span class="sr-only">(current)</span></a>
+            </li>
+            @cannot('is-admin')
+            <li class="nav-item active">
+                <a class="nav-link"  href="/applications/myApplications"> My Applications <span class="sr-only">(current)</span></a>
+            </li>
+            @endcannot
+         
+            @can('is-admin')
+            <li class="nav-item active">
+                <a class="nav-link"  href="/applications">Applications <span class="sr-only">(current)</span></a>
+            </li>
 
-                        </ul>
+            <li class="nav-item active">
+                <a class="nav-link"  href="{{route('admin.users.index') }} ">Users<span class="sr-only">(current)</span></a>
+            </li>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ms-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
+            <li class="nav-item active">
+                <a class="nav-link"  href="/jobs/create">Create A Job<span class="sr-only">(current)</span></a>
+            </li>
+            @endcan
+             </ul>
+             </div>
+             <ul class="navbar-nav ms-auto">
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
 
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }}
-                                    </a>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
 
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/">Profile</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
-        </div>
-    </div>
-</nav>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+   
+      
+      </nav>
 
 
         <main class="container">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,17 @@
 <div class="flex-center position-ref full-height">
 
 
-            <div class="content">
-                <img src="/img/teaching.jpg">
-                <div class="title m-b-md">
-                    Aston University Flex
-                </div>
-                @can('is-admin')
-                <p class="mssg">{{ session('mssg') }} </p>
-                <a href=" {{route('jobs.create') }}"> Create A Job</a>
-                @endcan
-                </div>
-
-            </div>
+    <div class="content">
+        <img src="/img/home-page.jpg">
+        <div class="title m-b-md">
+            Aston University Flex
         </div>
+        @can('is-admin')
+        <p class="mssg">{{ session('mssg') }} </p>
+        <a href=" {{route('jobs.create') }}"> Create A Job</a>
+        @endcan
+        </div>
+
+    </div>
+</div>
 @endsection
