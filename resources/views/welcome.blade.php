4b1b2ca206f365300295159d4ab4507258596d99
tony2490 <tonyyugbovwre@gmail.com>
2023-03-03 00:48:46 +0000

    Updated home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,14 +13,35 @@
                 <a  href="{{ route('login') }}"class="btn">Sign Up Now</a>
             </div>
         </section>
+        <div class="home-stripe">
+            <h3 class="home-stripe ">Teaching Assitant Roles For Both Final Year & PHD Aston Students</h3>
+        </div>
 
-        <main>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nibh molestie, efficitur leo sed, viverra nunc. Donec vehicula accumsan erat facilisis ullamcorper. Donec commodo quis dui nec placerat. Donec mi orci, scelerisque eget nisl ac, hendrerit condimentum odio. Nam dictum odio eget quam tempus, a mattis odio ornare. Nullam auctor libero ut libero suscipit, ut accumsan nunc condimentum. Donec ullamcorper maximus sapien quis egestas.</p>
+            <div class="card-group">
+                <div class="card">
+                    <img class="card-img-top" src="/img/teaching.jpg" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Benefit 1</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="/img/teaching.jpg" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Benefit 2</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="/img/teaching.jpg" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Benefit 3</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                </div>
+            </div>
+
     
-            <p>Mauris viverra scelerisque lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ultrices enim sit amet elit tincidunt maximus. Suspendisse vitae pellentesque lectus. Duis commodo leo suscipit augue mollis, non venenatis dolor ullamcorper. Duis tincidunt scelerisque lacus, vel vehicula leo consectetur vel. Duis posuere nisl non odio consequat ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            
-            <p>Etiam a leo nec mi blandit euismod. Etiam fringilla odio vitae risus ornare, id bibendum velit consequat. Fusce posuere risus sollicitudin condimentum ultrices. Fusce gravida, purus eget laoreet mattis, velit sapien ultrices diam, id dapibus erat leo id quam. Maecenas quis risus convallis, placerat elit non, iaculis tortor. Nullam porttitor magna risus, quis bibendum metus tincidunt in. Etiam vel ligula ac risus mattis tincidunt vel sit amet ante. Morbi et viverra ligula. Ut ac dignissim nisi, condimentum imperdiet mauris. Pellentesque ut ipsum vel diam tristique faucibus eu et lectus. Maecenas posuere neque non lacus bibendum, sit amet pharetra justo semper. Sed mi risus, tempor sit amet ligula eget, varius pretium est. Sed a odio in orci accumsan pretium suscipit ut quam.</p>
-        </main>
 
     </body>
 @endsection
