ef760bb30e43735880597a92bb1e02bad281b59c
tony2490 <tonyyugbovwre@gmail.com>
2023-03-11 01:22:45 +0000

    fix admin and users select box position

--- resources/views/Admin/Users/partials/form.blade.php
+++ resources/views/Admin/Users/partials/form.blade.php
@@ -31,37 +31,44 @@
 </div>
 
 @isset($create)
-<!-- <div class="row mb-3">
-    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+<!-- lll -->
+
+<div class="row mb-3">
+    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
     <div class="col-md-6">
-        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" 
+        value="{{ old('email') }} @isset($user) {{$user->email}} @endisset">
 
-        @error('password')
+        @error('email')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
         @enderror
     </div>
+</div>
 
-</div> -->
-
-<!-- <div class="row mb-3">
-    <label for="password_confirmation" class="col-md-4 col-form-label text-md-end">Password Confirm</label>
-
-    <div class="col-md-6">
-        <input name="password" id="password" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" required autocomplete="new-password">
-
-        @error('password_confirmation')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-    </div>
-
-</div> -->
 @endisset
-<div class="mb-3">
+
+
+<div class="row mb-3">
+@foreach($roles as $role)
+    <label  for= "{{$role->name}}" class="col-md-4 col-form-label text-md-end">    {{$role->name}} </label>
+
+    <div class="col-md-6">
+        <input iclass="form-check-input" name="roles[]" type="checkbox" value="{{$role->id}}" id="{{$role->name}}"
+        @isset($user) @if(in_array($role->id, $user->roles->pluck('id')->toArray())) checked @endif @endisset>
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+    @endforeach
+</div>
+
+
+<!-- <div class="mb-3">
     @foreach($roles as $role)
     <div class="form-check">
         <input  class="form-check-input" name="roles[]"
@@ -72,7 +79,7 @@
         </label>
     </div>
     @endforeach
-</div>
+</div> -->
 
 
 <div class="row mb-0">
