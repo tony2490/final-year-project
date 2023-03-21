d5bf0b8254ba489bd87199da3d8eb17da6b57b39
tony2490 <tonyyugbovwre@gmail.com>
2023-03-21 13:38:41 +0000

    Aston white logo update
    Changed navbar logo to white version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
 
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <a class="navbar-brand" href="/">
-            <img src="\img\aston-logo.svg" alt="" height="36">
+            <img src="\img\aston-logo-white.png" alt="" height="40">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
