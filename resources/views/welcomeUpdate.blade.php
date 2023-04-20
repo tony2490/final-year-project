ba149b7822f4b55c26502be5ee422d38ee98307e
tony2490 <tonyyugbovwre@gmail.com>
2023-04-20 19:05:10 +0100

    Update welcomeUpdate.blade.php

--- resources/views/welcomeUpdate.blade.php
+++ resources/views/welcomeUpdate.blade.php
@@ -2,10 +2,12 @@
 @section('content')
 
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="stylesheet" href="css/style.css" >
     </head>
 
     <body class="home-page">
+        <div class="main-content">
         <section class="hero">
             <div class="hero-inner">
                 <h1>Aston University Teaching Assistants</h1>
@@ -14,27 +16,25 @@
             </div>
         </section>
         <div class="home-stripe">
-            <h3 >Teaching Assistant Roles For Both Final Year & PHD Aston Students</h3>
+            <h3 >Teaching Assistant Roles For Both Final Year & PhD Aston Students</h3>
         </div>
         
         <div class="homepage-content">
-            <div class="card-group homepage-content">
-                <div class="card">
+            <div class="card-group homepage-content " style="width: 100%; padding-left: 70px;">
+                <div class="card card-responsive col-sm-6 col-md-4 col-lg-4 ">
                     <img class="card-img-top" src="/img/money.svg" alt="hourly pay">
                     <div class="card-body">
                         <h5 class="card-title">Paid Hourly</h5>
                         <p class="card-text">Teaching Assistant Roles are paid at an hourly rate. (Please see job description for accurate rates).</p>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="/img/school.svg" alt="invaluable experience">
+                <div class="card card-responsive col-sm-6 col-md-4 col-lg-34">                    <img class="card-img-top" src="/img/school.svg" alt="invaluable experience">
                     <div class="card-body">
                     <h5 class="card-title">An Invaluable Experience</h5>
                     <p class="card-text">Gain an invaluable experience which can work to your advantage when applying for jobs</p>
                     </div>
                 </div>
-                <div class="card">
-                    <img class="card-img-top" src="/img/balance.svg" alt="balance work and studies">
+                <div class="card card-responsive col-sm-6 col-md-4 col-lg-4 ">                    <img class="card-img-top" src="/img/balance.svg" alt="balance work and studies">
                     <div class="card-body">
                     <h5 class="card-title">Great Work-Life Balance</h5>
                     <p class="card-text">With capped hours for each role, you are able to focus on your individual learning progress too!</p>
@@ -44,15 +44,14 @@
             <!-- <strong> <div class="underline homepage-content" ></div> </strong> -->
 
             <div class="homepage-cards-content homepage-p">
-                <p> <img src="/img/aston-our-teaching-staff-ui.jpg" class="img-to-right " alt="" width="600" height="300" >
+                <p> <img src="https://www.aston.ac.uk/sites/default/files/2020-12/aston-our-teaching-staff-ui.jpg" class="img-to-right " alt="" width="600" height="300" >
 
                      <h2 class="content-title">What Work Will I Be Doing?</h2> <br>
                     <p">
                     The primary focus of this website is to assist with the allocation of part time teaching
                     assistant roles to modules that are searching for additional teaching support. This therefore
-                    means that each module will require a taeching assistant to be able to different activities 
-                    and may require different skills. We strongly recommend that you thorougly read through 
-                    each job description.  
+                    means that each module will require a teaching assistant to be able to different activities 
+                    and may require different skills.
                  <br> <br>
                  <a class="btn btn-sm btn-primary" href="/jobs" role="button"> View Jobs </a>
 
@@ -63,11 +62,11 @@
             <div style="padding-top: 80px;"></div>
 
             <div class="homepage-cards-content homepage-p">
-                <p> <img src="/img/teaching.jpg" class="img-to-left " alt="" width="600" height="300">
+                <p> <img src="https://www.oncampus.global/ugc-1/gallery/417/417_full.jpg" class="img-to-left " alt="" width="600" height="300">
 
                 <h2 class="content-title">Why Sign Up?</h2> <br>
                     <p">
-                        If you are a PhD or final yesr student at Aston university, signing up will allow you to
+                        If you are a PhD or final year student at Aston university, signing up will allow you to
                         view and apply the latest teaching assistant roles avaliable. This incredible opportunity 
                         can prove to be an important experience to add to your curriculum vitae, and you may gain 
                         invaluable skills.
@@ -83,16 +82,8 @@
 
             <div style="padding-bottom: 130px;"></div>
 
-       
-
-
-
-    
-
-   
-   
+        <!-- Testimonials -->
             
-
             <section class="home-testimonial homepage-content">
                 <div class="container-fluid">
                     <div class="row d-flex justify-content-center testimonial-pos">
@@ -100,26 +91,26 @@
                             <h3>Testimonials</h3>
                         </div>
                         <div class="col-md-12 d-flex justify-content-center">
-                            <h2>The Teaching Assistant Experience </h2>
+                            <h2>The Teaching Assistant Experience</h2>
                         </div>
                     </div>
                     <section class="home-testimonial-bottom">
                         <div class="container testimonial-inner">
                             <div class="row d-flex justify-content-center">
                                 <div class="col-md-4 style-3">
-                                    <div class="tour-item ">
+                                    <div class="tour-item">
                                         <div class="tour-desc bg-white">
-                                            <div class="tour-text color-grey-3 text-center">&ldquo;  Working as a part time teaching assistant allowed me to improve my communication skills and be able to help others! &rdquo;</div>
-                                            <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="/img/person.svg"  alt=""></div>
+                                            <div class="tour-text color-grey-3 text-center">&ldquo; Working as a part-time teaching assistant allowed me to improve my communication skills and be able to help others! &rdquo;</div>
+                                            <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="/img/person.svg" alt=""></div>
                                             <div class="link-name d-flex justify-content-center">Jonothan Davies</div>
                                             <div class="link-position d-flex justify-content-center">Student</div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-4 style-3">
-                                    <div class="tour-item ">
+                                    <div class="tour-item">
                                         <div class="tour-desc bg-white">
-                                            <div class="tour-text color-grey-3 text-center">&ldquo; With this teaching assistant role now on my cv I have been able to showcase my experiences and have been offered a graduate role! &rdquo;</div>
+                                            <div class="tour-text color-grey-3 text-center">&ldquo; With this teaching assistant role now on my CV, I have been able to showcase my experiences and have been offered a graduate role! &rdquo;</div>
                                             <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people"src="/img/person.svg" alt=""></div>
                                             <div class="link-name d-flex justify-content-center">Sarah Corbin</div>
                                             <div class="link-position d-flex justify-content-center">Student</div>
@@ -127,9 +118,9 @@
                                     </div>
                                 </div>
                                 <div class="col-md-4 style-3">
-                                    <div class="tour-item ">
+                                    <div class="tour-item">
                                         <div class="tour-desc bg-white">
-                                            <div class="tour-text color-grey-3 text-center">&ldquo; Working with the proffesor as a part time teaching assistant has allowed me to also build on my knowledge which has benefited me far in my final year &rdquo;</div>
+                                            <div class="tour-text color-grey-3 text-center">&ldquo; Working with the professor as a part-time teaching assistant has allowed me to also build on my knowledge, which has benefited me so far in my final year &rdquo;</div>
                                             <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="/img/person.svg" alt=""></div>
                                             <div class="link-name d-flex justify-content-center">Lisa White</div>
                                             <div class="link-position d-flex justify-content-center">Student</div>
@@ -137,16 +128,23 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
                     </section>
+                </div>
             </section>
+            
+            
             
 
         </div>
 
         <div  class="home-footer-padding"></div>
-
+</div>
     </body>
-@include('layouts.footer')
+    <footer>
+        @include('layouts.footer')
+
+    </footer>
 
 
 @endsection
